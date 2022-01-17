016cdfc67f410ad672b97386f10e9cbb55a24bb9
Viviane Puga <vnpuga@gmail.com>
2022-01-17 18:13:31 -0300

    requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
-
+// o template literal considera os espaços, e com a identação não passava no test, com orientação do Zezé, tirei a identação e o requisito passou.
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  } 
+return `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
+console.log(vqv('Tunico', 30));
 module.exports = vqv;
